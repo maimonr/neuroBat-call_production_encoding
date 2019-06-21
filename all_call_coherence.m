e4e09ee9c089cbdd16559e315108d29db305f097
Unknown <maimon.rose@gmail.com>
2019-06-21 11:03:11 -0700

    Code refactoring
    Making sub functions more modular

--- all_call_coherence.m
+++ all_call_coherence.m
@@ -78,8 +78,10 @@ params.specParams = struct('winSize',winSize,'overlap',overlap,'nfft',nfft,'f_bo
 neural_params.lfp_freq_band = [85 115];
 params.neural_params = neural_params;
 
+stabilityBounds = get_stability_bounds(batParams,params);
+
 [call_ts, cut_call_data, all_call_timestamps, included_call_times,...
-    included_call_ks] = get_call_timestamps(batParams,cut_call_data,params);
+    included_call_ks] = get_call_timestamps(cut_call_data,stabilityBounds,params);
 
 if ~neural_params.call_neural_common_reference
     min_ts = min([timestamps call_ts]) - 1;
@@ -640,7 +642,7 @@ end
 
 end
 
-function [call_ts, cut_call_data, all_call_timestamps, included_call_times, included_call_ks] = get_call_timestamps(batParams,cut_call_data,params)
+function stabilityBounds = get_stability_bounds(batParams,params)
 
 if strcmp(batParams.expType,'juvenile')
     
@@ -657,43 +659,6 @@ elseif any(strcmp(batParams.expType,{'adult','adult_operant'}))
     stabilityBounds = [-Inf Inf];
     
 end
-
-callpos = vertcat(cut_call_data.corrected_callpos);
-
-usable_call_idx = callpos(:,1) - params.call_time_offset >= stabilityBounds(1) &...
-    callpos(:,2) + params.call_time_offset <= stabilityBounds(2) &...
-    callpos(:,2) + params.call_time_offset <= params.max_t;
-
-cut_call_data = cut_call_data(usable_call_idx);
-callpos = callpos(usable_call_idx );
-
-all_call_timestamps = cell(1,length(cut_call_data));
-included_call_times = cell(1,length(cut_call_data));
-included_call_ks = cell(1,length(cut_call_data));
-
-k = 1;
-call_k = 1;
-
-while call_k <= length(cut_call_data)
-    call_time = round(cut_call_data(call_k).corrected_callpos(1));
-    
-    included_call_times{k} = call_time;
-    included_call_ks{k} = call_k;
-    call_k = call_k + 1;
-    while any((included_call_times{k}(end) + params.call_time_offset) >= callpos(call_k:end,1) - params.call_time_offset)
-        call_time = round(cut_call_data(call_k).corrected_callpos(1));
-        included_call_times{k} = [included_call_times{k} call_time];
-        included_call_ks{k} = [included_call_ks{k} call_k];
-        call_k = call_k + 1;
-    end
-    
-    last_call_length = round(abs(diff(cut_call_data(included_call_ks{k}(end)).corrected_callpos)));
-    call_start_idx = included_call_times{k}(1)-params.call_time_offset;
-    call_end_idx = included_call_times{k}(end)+last_call_length+params.call_time_offset-1;
-    all_call_timestamps{k} = call_start_idx:call_end_idx;
-    k = k + 1;
-end
-call_ts = [all_call_timestamps{:}];
 
 end
 
--- get_call_timestamps.m
+++ get_call_timestamps.m
@@ -1,11 +1,10 @@
-function [call_ts, cut_call_data, all_call_timestamps, included_call_times, included_call_ks] = get_call_timestamps(cut_call_data,stabilityBounds,callParams)
+function [call_ts, cut_call_data, all_call_timestamps, included_call_times, included_call_ks] = get_call_timestamps(cut_call_data,stabilityBounds,params)
 
 callpos = vertcat(cut_call_data.corrected_callpos);
 
-usable_call_idx = callpos(:,1) - callParams.call_time_offset >=0 &...
-    callpos(:,1) - callParams.call_time_offset >= stabilityBounds(1) &...
-    callpos(:,2) + callParams.call_time_offset <= stabilityBounds(2) &...
-    callpos(:,2) + callParams.call_time_offset <= callParams.max_t;
+usable_call_idx = callpos(:,1) - params.call_time_offset >= stabilityBounds(1) &...
+    callpos(:,2) + params.call_time_offset <= stabilityBounds(2) &...
+    callpos(:,2) + params.call_time_offset <= params.max_t;
 
 cut_call_data = cut_call_data(usable_call_idx);
 callpos = callpos(usable_call_idx );
@@ -23,13 +22,17 @@ while call_k <= length(cut_call_data)
     included_call_times{k} = call_time;
     included_call_ks{k} = call_k;
     call_k = call_k + 1;
-    while any((included_call_times{k}(end) + callParams.call_time_offset) >= callpos(call_k:end,1) - callParams.call_time_offset)
+    while any((included_call_times{k}(end) + params.call_time_offset) >= callpos(call_k:end,1) - params.call_time_offset)
         call_time = round(cut_call_data(call_k).corrected_callpos(1));
         included_call_times{k} = [included_call_times{k} call_time];
         included_call_ks{k} = [included_call_ks{k} call_k];
         call_k = call_k + 1;
     end
-    all_call_timestamps{k} = included_call_times{k}(1)-callParams.call_time_offset:included_call_times{k}(end)+callParams.call_time_offset-1;
+    
+    last_call_length = round(abs(diff(cut_call_data(included_call_ks{k}(end)).corrected_callpos)));
+    call_start_idx = included_call_times{k}(1)-params.call_time_offset;
+    call_end_idx = included_call_times{k}(end)+last_call_length+params.call_time_offset-1;
+    all_call_timestamps{k} = call_start_idx:call_end_idx;
     k = k + 1;
 end
 call_ts = [all_call_timestamps{:}];
